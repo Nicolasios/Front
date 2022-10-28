422163f35d64cf20a301baa93181352f725f5650
Nicolasios <1498913522@qq.com>
2022-10-28 17:28:50 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import RainbowText from "./Components/RainbowText";
+import ShineGradButton from "./Components/ShineGradButton";
+import CardClock from "./Components/CardClock";
+
+
+const App = ()=>{
+
+    return (
+        <>
+            <CardClock/>
+        </>
+    );
+};
+
+export  default  App;
